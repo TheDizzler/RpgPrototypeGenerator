e6607896b8365da1f918476a4fe1619698a4f798
TheDizzler <tgillon.music@gmail.com>
2020-12-21 20:01:04 +0900

    global suppressions have to be in root folder (below /Assets/)  to work

---
+++ RoleplayingPrototypeEngine/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "It's a stupid microsoft convention", Scope = "module")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0016:Use 'throw' expression", Justification = "Yeah, cause making something ugly is 'simple'", Scope = "namespace", Target= "AtomosZ.UniversalEditorTools.NodeGraph.Connections")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "module")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "module")]
+
