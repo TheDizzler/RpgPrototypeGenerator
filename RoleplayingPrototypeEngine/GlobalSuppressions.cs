2a891b9618aa8a75c83a1d9328e5be57e5fbb517
TheDizzler <tgillon.music@gmail.com>
2021-01-15 18:01:05 +0900

    update to global suppressions...these don't even work?

--- RoleplayingPrototypeEngine/GlobalSuppressions.cs
+++ RoleplayingPrototypeEngine/GlobalSuppressions.cs
@@ -4,7 +4,8 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "It's a stupid microsoft convention", Scope = "module")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0016:Use 'throw' expression", Justification = "Yeah, cause making something ugly is 'simple'", Scope = "namespace", Target= "AtomosZ.UniversalEditorTools.NodeGraph.Connections")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0016:Use 'throw' expression", Justification = "Yeah, cause making something ugly is 'simplified'", Scope = "module")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "Dumb microsoft shit", Scope = "module")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "module")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "module")]
 
